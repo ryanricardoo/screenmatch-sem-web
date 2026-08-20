48f9310c0c4a3dae7584d6f21df32992f1435f15
Ryan Ricardo <ricardoryan2403@gmail.com>
2026-08-20 17:40:37 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception{
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?i=tt3896198&apikey=6c7e64d4");
+        System.out.println(json);
+
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println("Dados Série: " + dados);
+        json = consumoApi.obterDados("https://www.omdbapi.com/?i=tt3896198&apikey=6c7e64d4");
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+        System.out.println("Dados Episódio: " + dadosEpisodio);
+    }
+}
---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch.service;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper maper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return maper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
